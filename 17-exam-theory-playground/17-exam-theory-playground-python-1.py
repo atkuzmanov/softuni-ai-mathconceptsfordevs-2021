00629e2102df953304ce50f2d9808244d457d672
atkuzmanov <atkuzmanov@gmail.com>
2021-05-16 19:14:13 +0300

    Work-In-Progress-WIP-2021-05-16-191413

---
+++ 17-exam-theory-playground/17-exam-theory-playground-python-1.py
@@ -0,0 +1,41 @@
+### Python imports
+# mostly standard imports
+import sympy
+import math
+import cmath
+import numpy as np
+import numpy.polynomial.polynomial as p
+import matplotlib.pyplot as plt
+from turtle import *
+import re
+from sympy.ntheory import discrete_log
+from matplotlib.transforms import Affine2D
+import skimage.io
+import time
+# imports for egienvalues and eigenvectors
+from numpy import linalg as LA
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA as sklearnPCA
+# imports for PCA
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import seaborn as sns
+# additional imports just in case 1
+import statsmodels
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from skimage.io import imread
+import scipy.ndimage
+import scipy.stats
+import scipy.io.wavfile
+from scipy.fftpack import fft, fftfreq
+
+
+
+
+
+
+
+
